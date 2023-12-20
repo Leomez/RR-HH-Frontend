f7d992771c8fdfefc84c7da65d4a6d5cd1316277
Leomez <leoariel17@gmail.com>
2023-12-20 11:54:22 -0300

    creo variable de entorno y modifico archivo de configuracion de firebase

--- src/Config/firebase.js
+++ src/Config/firebase.js
@@ -1,20 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getStorage } from "firebase/storage"
+import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MASSAGIN_SENDER_ID,
+  VITE_APP_ID,
+  VITE_MEASUREMENT_ID,
+} = import.meta.env;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDT7o1vsaeLxOLhD2I0V9prmN3D8Y6Fn84",
-  authDomain: "coq-recursos-humanos.firebaseapp.com",
-  projectId: "coq-recursos-humanos",
-  storageBucket: "coq-recursos-humanos.appspot.com",
-  messagingSenderId: "460342143854",
-  appId: "1:460342143854:web:9ba9f777cae37ed0cf2f7f",
-  measurementId: "G-GR1NSWME6F"
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MASSAGIN_SENDER_ID,
+  appId: VITE_APP_ID,
+  measurementId: VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -22,4 +31,4 @@ const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const storage = getStorage(app);
 
-export {app, storage, analytics};
+export { app, storage, analytics };
