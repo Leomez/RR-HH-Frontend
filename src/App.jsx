f672fbb59a936bbc0450fe628908d239246b150e
Leomez <leoariel17@gmail.com>
2024-03-06 14:01:23 -0300

    agrego actions para activar loading y errores en algunos slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import './App.css'
 import { Box } from '@mui/material'
+import { useDispatch } from 'react-redux'
 import { Layout } from './Componentes/Layout/Layout'
 import { AppRouter } from './Routes/AppRouter'
+import { showError } from './Redux/Features/Error/errorSlice'
 // import { ErrorModal } from './Componentes/ErrorModal'
 
 
 function App() {
-  return (
-    <Box>
-      <Layout>
-        {/* <ErrorModal /> */}
-        <AppRouter />
-      </Layout>
-    </Box>
-  )
+  const dispatch = useDispatch()
+  try {
+    return (
+      <Box>
+        <Layout>
+          {/* <ErrorModal /> */}
+          <AppRouter />
+        </Layout>
+      </Box>
+    )    
+  } catch (error) {
+    console.log(error);
+    dispatch(showError(error))
+  }
 }
 
 export default App
