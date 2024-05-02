71c914d3c0fc5fce027b12e4729e046ce5b1f601
Leomez <leoariel17@gmail.com>
2024-05-02 13:15:34 -0300

    muestro dias feriados en el calendario chico

--- src/Pages/LicenciasYPermisos/index.jsx
+++ src/Pages/LicenciasYPermisos/index.jsx
@@ -1,29 +1,53 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
 import { Typography } from "@mui/joy"
 import { Box, Button, Divider, Drawer } from "@mui/material"
 import CalendarioChico from "../../Servicios/Licencias/CalendarioChico"
 import CalendarioGrande from "../../Servicios/Licencias/CalendarioGrande"
 import Formulario from "../../Servicios/Licencias/Formulario"
-import { useState, useEffect } from "react"
 import dayjs from "dayjs"
 // import { Typography } from "@mui/material"
 
 Typography
 
 export function LicenciasYPermisos() {
+    const [feriados, setFeriados] = useState([])
+    const [anio, setAnio] = useState(dayjs().year())
     const [marcador, setMarcador] = useState(dayjs())
     const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    async function fetchFeriados(anio) {
+        try {
+            setLoading(true)
+            const response = await axios.get(`https://api.argentinadatos.com/v1/feriados/${anio}`)
+            setFeriados(response.data)
+            setLoading(false)
+        } catch (error) {
+            setFeriados([])
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchFeriados(anio)
+    }, [anio])
+
     const handlerMarcador = (e) => {
         setMarcador(e)
+        // setAnio(e.year())
     }
-   
+
+    // console.log(feriados);
+
     useEffect(() => {
         setMarcador(dayjs())
     }, [])
 
     const close = () => {
-        setOpen(false)        
+        setOpen(false)
     }
-    
+
     return (
         <div>
             <Typography px={2} fontWeight={400} lineHeight={2} level="h4">LICENCIAS Y PERMISOS</Typography>
@@ -42,10 +66,17 @@ export function LicenciasYPermisos() {
                     <Button onClick={() => setOpen(true)} sx={{ margin: '1rem' }} variant='contained'> Solicitar </Button>
                     <Drawer anchor='right' open={open} onClose={() => setOpen(false)}>
                         <Formulario close={close}
-                    />
+                        />
                     </Drawer>
                     <Divider />
-                    <CalendarioChico marcador={marcador} handler={handlerMarcador} />
+                    <CalendarioChico
+                        marcador={marcador}
+                        feriados={feriados}
+                        anio={anio}
+                        setAnio={setAnio}
+                        loading={loading}
+                        handler={handlerMarcador}
+                    />
                 </Box>
                 <Divider orientation="vertical" flexItem />
                 <Box sx={{ padding: 1, width: '100%' }}>
---
+++ src/Servicios/Licencias/utils/TimePickerSelectivo.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { TimePicker } from '@mui/x-date-pickers'
+import dayjs from 'dayjs'
+
+
+const llegarTarde = ({ formData, setFormData }) => {
+    return <TimePicker
+        label='Hora de entrada'
+        ampm={false}
+        defaultValue={dayjs()}
+        minTime={dayjs().set('hour', '08').startOf('hour')}
+        maxTime={dayjs().set('hour', '20').startOf('hour')}
+        format="HH:mm"
+        onChange={(newValue) => setFormData({ ...formData, horaPermiso: newValue.format('HH:mm:00') })}
+    />
+}
+
+const salirTemprano = ({ formData, setFormData }) => {
+    return <TimePicker
+        label='Hora de salida'
+        ampm={false}
+        defaultValue={dayjs()}
+        minTime={dayjs().set('hour', '08').startOf('hour')}
+        maxTime={dayjs().set('hour', '20').startOf('hour')}
+        format="HH:mm"
+        onChange={(newValue) => setFormData({ ...formData, horaPermiso: newValue.format('HH:mm:00') })}
+    />
+}
+
+const CambioHorario = ({ formData, setFormData }) => {
+    return <TimePicker
+        label='Nueva hora de entrada'
+        ampm={false}
+        defaultValue={dayjs()}
+        minTime={dayjs().set('hour', '08').startOf('hour')}
+        maxTime={dayjs().set('hour', '20').startOf('hour')}
+        format="HH:mm"
+        onChange={(newValue) => setFormData({ ...formData, horaPermiso: newValue.format('HH:mm:00') })}
+    />
+}
+
+function TimePickerSelectivo(props) {
+    const { formData, setFormData, categoria } = props
+    console.log('estoy en TimePickerSelectivo');
+    switch (categoria) {
+        case 'Llegar tarde':
+            return llegarTarde({ formData, setFormData })
+        case 'Salir temprano':
+            return salirTemprano({ formData, setFormData })
+        case 'Cambio de horario':
+            return CambioHorario({ formData, setFormData })
+        default:
+            return null
+    }
+
+}
+
+export default TimePickerSelectivo
